a85449d1076021c283cd77f7c4b8029c2d3db796
fatehi AL-AGHBARI <agh.fatehi@yahoo.com>
2026-06-06 12:59:49 +0300

    refactor: Enhance QR code rendering logic with auto-detection of available libraries

--- resources/views/qr-code.blade.php
+++ resources/views/qr-code.blade.php
@@ -1,2 +1,18 @@
-{{-- Uses built-in SVG QR generator when endroid/qr-code is not installed --}}
-{!! app(\Aghfatehi\Zatca\Services\SvgQrGenerator::class)->generate($qrData, $size) !!}
+{{-- QR code renderer: auto-detects the best available library --}}
+@php
+    $qrData = $qrData ?? '';
+    $size = $size ?? 200;
+
+    if (class_exists(\SimpleSoftwareIO\QrCode\Facades\QrCode::class)) {
+        echo \SimpleSoftwareIO\QrCode\Facades\QrCode::size($size)->generate($qrData);
+    } elseif (class_exists(\Endroid\QrCode\QrCode::class)) {
+        $qr = \Endroid\QrCode\QrCode::create($qrData)
+            ->setSize($size)
+            ->setMargin(4)
+            ->setErrorCorrectionLevel(\Endroid\QrCode\ErrorCorrectionLevel::High);
+        $writer = new \Endroid\QrCode\Writer\SvgWriter();
+        echo $writer->write($qr)->getString();
+    } else {
+        echo app(\Aghfatehi\Zatca\Services\SvgQrGenerator::class)->generate($qrData, $size);
+    }
+@endphp
